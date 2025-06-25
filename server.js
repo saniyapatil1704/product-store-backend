fdd548429faa0248560f6f95bb00f233fe85408a
Saniya Mahesh Patil <patilsaniya172004@gmail.com>
2025-06-25 09:34:47 +0530

    Made changes in server.js

--- server.js
+++ server.js
@@ -1,32 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"; // ✅ use import not require
 import { connectDB } from "./config/db.js";
-// import Product from "./models/product.models.js";
-// import mongoose from "mongoose";
 import productRoutes from "./routes/product.route.js";
-const cors = require("cors");
-
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors({
-  origin: "https://product-store-murex.vercel.app", // allow your frontend domain
+  origin: "https://product-store-murex.vercel.app", // ✅ allow Vercel frontend
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // allows us to accept JSON data in the req.body
+app.use(express.json());
 
-app.use("/api/products",productRoutes)
+app.use("/api/products", productRoutes);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server started at http://localhost:" + PORT);
   });
 });
-
-// rrQT2P3oSPeLL00U
